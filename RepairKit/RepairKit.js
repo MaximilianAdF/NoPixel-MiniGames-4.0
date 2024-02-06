ae1249081109693a691591751298ac78ce542755
Maximilian Alvim de Faria <63980031+MaximilianAdF@users.noreply.github.com>
2024-02-06 20:15:57 +0100

    Finished RepairKit Minigame
    ReadMe will be updated with instrucitons as well as a link to the github pages with the working version asap

--- RepairKit/RepairKit.js
+++ RepairKit/RepairKit.js
@@ -1,10 +1,59 @@
+var timerInterval = null;
+var slotPosition = 0;
+var currPosition = 1;
+function startGame() {
+    var movingSquare = document.querySelector('.moving-square');
+    movingSquare.style.background = "radial-gradient(circle, rgb(0, 255, 200), rgb(0, 174, 130))";
+    movingSquare.style.boxShadow = "0 0 5px 0px rgb(16, 239, 191)";
+    movingSquare.style.marginLeft = "1%";
+    currPosition = 0;
+    randomizeSquareSlot();
+    tick();
+}
+function checkWin() {
+    var movingSquare = document.querySelector('.moving-square');
+    if (timerInterval) {
+        clearInterval(timerInterval);
+    }
+    if (slotPosition - 2 <= currPosition && currPosition <= slotPosition + 2) {
+        movingSquare.style.scale = '1.2';
+        setTimeout(function () {
+            movingSquare.style.scale = '1';
+        }, 300);
+        return true;
+    }
+    movingSquare.style.background = "radial-gradient(circle, rgb(255, 85, 76), rgba(132, 32, 32, 0.894))";
+    movingSquare.style.boxShadow = "0 0 5px 0px rgb(255, 0, 0)";
+    return false;
+}
+function tick() {
+    var movingSquare = document.querySelector('.moving-square');
+    timerInterval = setInterval(function () {
+        movingSquare.style.marginLeft = "".concat(currPosition, "%");
+        currPosition += 1;
+        if (currPosition > Math.min(slotPosition + 7, 94)) {
+            if (timerInterval) {
+                clearInterval(timerInterval);
+            }
+            handleKeyPress(new KeyboardEvent('keydown', { key: 'E' }));
+        }
+    }, 50);
+}
+function handleKeyPress(event) {
+    if (event.key === 'e' || event.key === "E") {
+        checkWin();
+        setTimeout(function () {
+            startGame();
+        }, 1000);
+    }
+}
 function randomizeSquareSlot() {
     var squareSlot = document.querySelector('.square-slot');
-    var position = Math.floor(Math.random() * (94 - 15) + 15);
-    squareSlot.style.marginLeft = "".concat(position, "%");
+    slotPosition = Math.floor(Math.random() * (90 - 15) + 15);
+    squareSlot.style.marginLeft = "".concat(slotPosition, "%");
     squareSlot.style.display = "flex";
-    return position;
 }
+document.addEventListener('keydown', handleKeyPress);
 document.addEventListener('DOMContentLoaded', function (event) {
-    var winPosition = randomizeSquareSlot();
+    startGame();
 });
--- RepairKit/RepairKit.ts
+++ RepairKit/RepairKit.ts
@@ -1,12 +1,64 @@
-function randomizeSquareSlot(): number {
-    const squareSlot = document.querySelector('.square-slot') as HTMLElement
-    const position = Math.floor(Math.random() * (94 - 15) + 15);
+let timerInterval: NodeJS.Timeout | null = null;
+let slotPosition = 0;
+let currPosition = 1;
 
-    squareSlot.style.marginLeft = `${position}%`;
-    squareSlot.style.display = `flex`
-    return position;
+function startGame(): void {
+    const movingSquare = document.querySelector('.moving-square') as HTMLElement;
+    movingSquare.style.background = `radial-gradient(circle, rgb(0, 255, 200), rgb(0, 174, 130))`;
+    movingSquare.style.boxShadow = `0 0 5px 0px rgb(16, 239, 191)`;
+    movingSquare.style.marginLeft = `1%`;
+    currPosition = 0;
+    randomizeSquareSlot();
+    tick();
 }
 
+function checkWin(): boolean {
+    const movingSquare = document.querySelector('.moving-square') as HTMLElement;
+    if (timerInterval) {clearInterval(timerInterval);}
+    if (slotPosition-2 <= currPosition && currPosition <= slotPosition+2) {
+        movingSquare.style.scale = '1.2';
+        setTimeout(() => {
+            movingSquare.style.scale = '1';
+        }, 300);
+
+        return true;
+    }
+    movingSquare.style.background = `radial-gradient(circle, rgb(255, 85, 76), rgba(132, 32, 32, 0.894))`;
+    movingSquare.style.boxShadow = `0 0 5px 0px rgb(255, 0, 0)`;
+    return false;
+}
+
+function tick(): void {
+    const movingSquare = document.querySelector('.moving-square') as HTMLElement;
+    timerInterval = setInterval(() => {
+        movingSquare.style.marginLeft = `${currPosition}%`;
+        currPosition+=1;
+        
+        if (currPosition > Math.min(slotPosition+7,94)) {
+            if (timerInterval) {clearInterval(timerInterval);}
+            handleKeyPress(new KeyboardEvent('keydown', { key: 'E' }));
+        }
+    }, 50);
+}
+function handleKeyPress(event: KeyboardEvent): void {
+    if (event.key === 'e' || event.key === "E") {
+        checkWin();
+        setTimeout(() => {
+            startGame();
+        },1000)
+    }
+}
+
+function randomizeSquareSlot(): void {
+    const squareSlot = document.querySelector('.square-slot') as HTMLElement;
+    slotPosition = Math.floor(Math.random() * (90 - 15) + 15);
+
+    squareSlot.style.marginLeft = `${slotPosition}%`;
+    squareSlot.style.display = `flex`;
+}
+
+document.addEventListener('keydown', handleKeyPress);
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    const winPosition = randomizeSquareSlot()
+    startGame();
 })
