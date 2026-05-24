85c1d1e6f83aa6d2c78bdacaec01818ff25aff9b
MaximilianAdF <MaximilianAdF@users.noreply.github.com>
2026-05-25 01:31:29 +0200

    chore: remove dead code
    - delete app/page-old.tsx (next.js template boilerplate, replaced by page.tsx)
    - delete app/components/DonateContainer.tsx (only ref was a commented block)
    - remove the commented-out DonateContainer block from home page

--- app/page.tsx
+++ app/page.tsx
@@ -523,12 +523,6 @@ export default function Home() {
             </Link>
           </div>
         </footer>
-        
-        {/* <DonateContainer title="Donate" description="If you like this project, consider donating to support development" className="absolute bottom-0 left-0 px-5 py-5 w-80">
-          <a href="https://www.buymeacoffee.com/MaximilianAdF" target="_blank" rel="noreferrer">
-              <img src={bmcLogo.src} alt="Buy Me A Coffee" className="rounded-lg bg-spring-green-300" style={{ height: 60 }} />
-          </a>
-        </DonateContainer> */}
 
       </main>
   );
